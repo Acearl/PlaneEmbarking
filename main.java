7d701f063ff8ae72bd14f089312d02ffc83c9ecc
Acearl <acearl57@gmail.com>
2025-03-17 23:29:55 -0400

    Display works as queues, generating passengers works for max people, and for who is initally at the airport terminal. This works now as not ArrayLists. Even though ArrayLists would be better in every way for this situation.

--- main.java
+++ main.java
@@ -5,13 +5,14 @@ import java.util.Random;
 import java.util.Queue;
 import java.util.LinkedList;
 //Collaberation between Andrew Earl and
+//hours spent 2.5 (mostly debugging)
 public class main
 {
 
 	static int planeSize = 20; 
 	static int numSeatTypes = 3;
-	static int[] passengerNums = new int[numSeatTypes];//tickets sold per class
-	static int[] passengerWaiting = new int[numSeatTypes];//current number of people per class waiting in line;
+	static Queue<Integer> passengerNums = new LinkedList<>();//tickets sold per class
+	static  Queue<Integer> passengerWaiting = new  LinkedList<>();//current number of people per class waiting in line;
 	ArrayList<Integer> plane = new ArrayList<>();
 	public static void main(String[] args)
 	{
@@ -20,11 +21,11 @@ public class main
 		System.out.println("test");
 
 		genPassengerNums();
-		System.out.println(Arrays.toString(passengerNums));
+		System.out.println(Arrays.toString(passengerNums.toArray()));
 		genWaiting();
-		System.out.println(Arrays.toString(passengerWaiting));
+		System.out.println(Arrays.toString(passengerWaiting.toArray()));
 		printPassWait();
-		System.out.println(Arrays.toString(passengerNums));
+		System.out.println(Arrays.toString(passengerNums.toArray()));
 	}
 	public static void genPassengerNums()
 	{
@@ -32,43 +33,45 @@ public class main
 		int leftover = planeSize;
 		System.out.println("size: " + planeSize);
 		//leftover < size for many more types of passengers. It would go on to the plane's limit of 1 kind of passenger.
-		for(int i = 0; i < passengerNums.length-1; i++) 	
-		{			
+		for(int i = 0; i < numSeatTypes-1; i++) 	
+		{	
+			
 			System.out.println("leftover: "+leftover);
 			int num = random.nextInt(2, leftover); //makes number of passengers
-			passengerNums[i] = num;
+			passengerNums.add(num);
 			leftover = leftover - num;
-		
 		}
 		System.out.println("leftover: "+leftover);
-		passengerNums[passengerNums.length - 1] = leftover;
+		passengerNums.add(leftover);
 	}
 	public static void genWaiting()
 	{
 		Random random = new Random();
-		for(int i = 0; i < passengerNums.length - 1; i++)
+		Queue<Integer> temp = new LinkedList<>(passengerNums);
+		int tempInt;
+		for(int i = 0; i <= numSeatTypes-1; i++)
 		{
-			int rand = random.nextInt(1,passengerNums[i]);
-			passengerWaiting[i] = rand;
+			System.out.print("pass peek: " + temp.peek());
+			tempInt = temp.remove();
+			int rand = random.nextInt(1,tempInt);
+			passengerWaiting.offer(rand);
+			System.out.println(" i: "+i+" temp: " + tempInt + " rand: " + rand);//Debug
 		}
 	}
 	public static void printPassWait()
 	{
 		String str = new String();
 		String strB = new String();
-		//for(int i = 0; i < passengerNums.length-1; i++)
-		//{
-			str = "#".repeat(planeSize+passengerNums.length+1);
-			System.out.println(str);
-			strB = "| ";
-			for(int y = 0; y <= passengerNums.length-1; y++)
-			{
-			
-				strB = strB + "-".repeat(passengerNums[y])+" ";
-			}
-			//strB = strB + "-".repeat(passengerNums[passengerNums.length-1])+" ";
-			System.out.println(strB);
-			System.out.println(str);
-		//}
+		Queue<Integer> temp = new LinkedList<>(passengerNums);
+		str = "#".repeat(planeSize+numSeatTypes+1);
+		System.out.println(str);
+		strB = "| ";
+		for(int y = 0; y <= numSeatTypes-1; y++)
+		{
+			int tempInt = temp.poll();
+			strB = strB + "-".repeat(tempInt)+" ";
+		}
+		System.out.println(strB);
+		System.out.println(str);
 	}
 }
